7d3dcfd1eec1c01f424a306a4e5ad60ae6c46eae
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-12 14:44:49 +0900

    新規登録バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください' }
+
   has_many :posts
   has_many :comments
 end
