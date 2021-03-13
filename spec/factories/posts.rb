adff3ddea11090609e0a08545c28f11bd4e25b7e
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-13 11:35:38 +0900

    テストコード作成

---
+++ spec/factories/posts.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :post do
+    title   { Faker::Lorem.sentence }
+    text    { Faker::Lorem.sentence }
+    area_id { 2 }
+    # score   { 5 }
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
