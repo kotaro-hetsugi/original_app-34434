3298d7d29d0422beaed2d5291baf8e7ee013debc
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-14 18:21:20 +0900

    destroyアクションの記入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -14,7 +14,6 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     unless @post.save
-      @post
       render :new
     end
   end
@@ -27,12 +26,12 @@ class PostsController < ApplicationController
 
   def update
     unless @post.update(post_params)
-      @post
       render :edit
     end
   end
 
   def destroy
+    @post.destroy
   end
 
   private
