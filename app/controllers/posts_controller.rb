1de57e0856c463c7df2bcb5a34b87b6faa5b059e
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-14 18:25:41 +0900

    投稿ユーザー以外の削除防止

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.includes(:user).order('created_at desc')
