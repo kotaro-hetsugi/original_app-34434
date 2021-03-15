3fc41162d181fb789143856f229c284b7d3e5ac9
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-15 19:40:54 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,12 @@ class Post < ApplicationRecord
   belongs_to :area
 
   validates :image, presence:{ message:'を投稿してください'}
-  validates :title, presence: true
+
+  with_options presence: true do
+    validates :title
+    validates :score
+  end
+
   validates :area_id, numericality: { other_than: 1, message:'を選択してください'}
 
   belongs_to :user
