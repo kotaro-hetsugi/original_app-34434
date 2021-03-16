ce1eb298230ef8d5cc3b686953d60b9f7b25222c
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-16 20:28:54 +0900

    foreign_keyの追加

--- db/schema.rb
+++ db/schema.rb
@@ -68,4 +68,6 @@ ActiveRecord::Schema.define(version: 2021_03_16_091559) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
