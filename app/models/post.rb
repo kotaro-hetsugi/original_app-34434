fb25b346b7ab6698db269033ffc70c82cadbd4a3
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-16 17:47:25 +0900

    Rubocop導入＆修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,14 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :area
 
-  validates :image, presence:{ message:'を投稿してください'}
+  validates :image, presence: { message: 'を投稿してください' }
 
   with_options presence: true do
     validates :title
     validates :score
   end
 
-  validates :area_id, numericality: { other_than: 1, message:'を選択してください'}
+  validates :area_id, numericality: { other_than: 1, message: 'を選択してください' }
 
   belongs_to :user
   has_many :comments
