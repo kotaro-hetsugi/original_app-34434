1a07cbae34b7d6533df6b506d2b2dfb0b96f7357
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-16 19:00:32 +0900

    コントローラ追記

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,19 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post.id)
+    else
+      @post
+      @comment
+      render 'posts/show'
+    end
+  end
+
+  private
   
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
   end
 
   def edit
