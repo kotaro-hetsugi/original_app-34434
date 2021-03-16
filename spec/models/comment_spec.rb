a92eca82f4ecfe3b5ba5918d13545eb73735696c
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-03-16 20:29:09 +0900

    テストコード作成

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    post = FactoryBot.create(:post)
+    @comment = FactoryBot.build(:comment, user_id: user.id, post_id: post.id)
+    sleep(0.1)
+  end
+
+  describe 'コメント投稿' do
+    context 'コメント投稿できる時' do
+      it 'コメントが入力されていれば投稿できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメント投稿できない時' do
+      it 'コメントが空だと投稿できない' do
+        @comment.text = ''
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('Textを入力してください')
+      end
+    end
+  end
 end
