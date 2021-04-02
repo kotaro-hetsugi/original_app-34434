88bd9fc424430909c655739efaeb3de90f33841b
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-04-02 17:53:49 +0900

    remenber_me有効化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def remember_me
+    true
+  end
 
   validates :nickname, presence: true
 
