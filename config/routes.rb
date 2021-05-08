5208eab4c668f59a3ee2d984398ee9193a138bb4
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-05-08 19:19:02 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :comments ,only: [:create]
+    resource :likes, only: [:create, :destroy]
     collection do
       get 'search'
       get 'search_candidate'
